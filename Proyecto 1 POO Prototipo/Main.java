e2fae62b52d11afeba0ac56a3953114dba65d3f9
Joel Chalé <joelchale31@gmail.com>
2026-05-16 19:10:49 -0600

    Interfaz

--- Proyecto 1 POO Prototipo/Main.java
+++ Proyecto 1 POO Prototipo/Main.java
@@ -7,20 +7,40 @@ public class Main {
 
         do {
 
-            System.out.println("1) Agregar usuario \n2) Salir");
+            System.out.println("--------------------------------------- \n1) Mostrar cleinte registrados \n2) Mostrar todas las facturas ");
+            System.out.println("\n3) Crear factura \n4) Mostrar facturas de un cliente \n5) Salir \n---------------------------------------");
             opcion = board.nextInt();
             System.lineSeparator();
 
 
             switch(opcion) {
-                case 1:
-
-                    Cliente.guardarCliente(board);
-
+                case 1 :
+                    mostrarClienteRegistrados();
                     break;
-                case 2: 
+
+                case 2 :
+                    Factura.cargarFacturas();
+                    break;
+
+                case 3 :
+                    Factura.crearFactura();
+                    break;
+
+                case 4 :
+                    Factura.filtrarFacturas();
+
+
+                case 5 : 
                     System.out.println("Saliendo");
                     System.exit(0);
+                    break;
+
+                case 6 :
+                    Cliente.guardarCliente(board);
+                    break;
+
+                default: 
+                    System.out.println("Seleccione una opcion valida");
                     break;
             }
 
